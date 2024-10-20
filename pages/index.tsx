13da457c48f0495a481208e70f1a292258ce44a7
Ahmed Younas <142696963+4hmed7ounas@users.noreply.github.com>
2024-10-21 02:27:31 +0500

    chore: change file name to mainmodel

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import KenoGamePlay from "@/components/game-display";
-import MainModel from "@/components/threejsmodel/threejsmodel";
+import MainModel from "@/components/threejsmodel/mainmodel";
 import { KenoPlayManagerProvider } from "@/contexts/game-managers/KenoPlayManagerContext";
 import React from "react";
 
