0077eb73c482ce3c6a8240f941d92a5b67b8d00b
Ahmed Younas <142696963+4hmed7ounas@users.noreply.github.com>
2024-11-01 03:18:18 +0500

    fix: fix background

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,10 +9,6 @@ function KenoGameDisplay() {
     //   <KenoGamePlay />
     //   <MainModel/>
     // </KenoPlayManagerProvider>
-
-    // I got some issue with upwork i have made alot of changes but i don't know where the whole code is where i have to make changes in
-    // in UI. and also where to put this model.
-    // unable to send msgs
     <div
       style={{
         margin: "0",
